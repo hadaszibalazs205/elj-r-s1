42958fb8665639f8c692120202335b90b9a5775a
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-02-03 17:17:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+1. Feladat
+Írj eljárást, amely egy tetszőleges szöveget, ill. alakzatot ír ki a képernyőre!
+'''
+
+def felirat():
+	      print('Ez itt a replit? Jó helyen járok?')
+          
+felirat()
